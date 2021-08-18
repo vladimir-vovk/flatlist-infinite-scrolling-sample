d948f68ed9d0a27b90f8315424de852120e07297
Vladimir Vovk <vladimir@vovk.in>
2021-08-18 12:37:12 +0300

    feat: add infinite scrolling

--- App.tsx
+++ App.tsx
@@ -1,21 +1,120 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react'
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native'
+import axios from 'axios'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const ITEM_HEIGHT = 40
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  flatlist: {
+    flex: 1
   },
-});
+  content: {
+    padding: 16
+  },
+  card: {
+    height: ITEM_HEIGHT,
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+    borderBottomColor: 'gray'
+  },
+  loadingMore: {
+    padding: 8
+  }
+})
+
+type Country = {
+  name: string
+}
+
+type RenderItemArgs = {
+  item: Country
+  index: number
+}
+
+const ENDPOINT = 'https://vvovk-countries-api.herokuapp.com/countries'
+const LIMIT = 20
+
+export default function App() {
+  const [data, setData] = useState<Country[]>([])
+  const [total, setTotal] = useState(0)
+  const [isLoadingMore, setLoadingMore] = useState(false)
+  const [offset, setOffset] = useState(0)
+
+  const endpointWithParams = () => `${ENDPOINT}?offset=${offset}&limit=${LIMIT}`
+
+  const fetchMoreData = () => {
+    setLoadingMore(true)
+    axios
+      .get(endpointWithParams())
+      .then((response) => {
+        /*
+        Response: {
+          total: number
+          data: [
+            { name: string }
+          ]
+        }
+      */
+        setTotal(response?.data?.total)
+        setOffset(offset + LIMIT)
+
+        const countries = response?.data?.data
+        setData([...data, ...countries])
+        setLoadingMore(false)
+      })
+      .catch((err) => {
+        console.log('Request error:', err)
+      })
+  }
+
+  useEffect(() => {
+    fetchMoreData()
+  }, [])
+
+  const renderItem = ({ item, index }: RenderItemArgs) => {
+    const { name } = item
+
+    return (
+      <View style={styles.card}>
+        <Text>{`${index} - ${name}`}</Text>
+      </View>
+    )
+  }
+
+  const keyExtractor = (item: Country) => item.name
+
+  const onEndReachedThreshold = 0.1
+
+  const onEndReached = () => {
+    if (offset + 1 < total) {
+      fetchMoreData()
+    }
+  }
+
+  const ListFooterComponent = () => {
+    if (isLoadingMore) {
+      return (
+        <View style={styles.loadingMore}>
+          <ActivityIndicator />
+        </View>
+      )
+    }
+
+    return <></>
+  }
+
+  return (
+    <FlatList
+      style={styles.flatlist}
+      contentContainerStyle={styles.content}
+      {...{
+        renderItem,
+        keyExtractor,
+        data,
+        onEndReachedThreshold,
+        onEndReached,
+        ListFooterComponent
+      }}
+    />
+  )
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# FlatList Infinite Scroll Example
+
+An example of infinite scrolling with React Native FlatList.
+
+<img src=".screen.gif" width="300"></img>
+
+## Installation
+
+1. Clone the repo
+2. Change directory to the app: `cd flatlist-infinite-scrolling-sample`
+3. Install all dependencies: `yarn install`
+4. Run the app: `yarn start`
+
+## Expo
+
+You can also run this app with [Expo Mobile Client](https://expo.io/@vladimir_vovk/flatlist-infinite-scrolling-sample) or [Expo Snack]() in your browser.
